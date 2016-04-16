3b2a48668a4f74ecf98d2f95779c33ac8faed5c4
RZeus <rajbirsinghshienh@gmail.com>
2016-04-15 17:56:25 -0700

    Connected Notepad Editor with Note List and some remaining files

---
+++ app/src/main/java/com/zeus/multiuseapp/listener/OnStartNewFragmentListener.java
@@ -0,0 +1,10 @@
+package com.zeus.multiuseapp.listener;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Zeus on 4/14/2016.
+ */
+public interface OnStartNewFragmentListener {
+    void onStartNewFragment(Fragment fragment, String title);
+}
